222fa77668176e24774d1ff03450af810f3127c4
superior-prog <meherajmahmmd@gmail.com>
2021-01-17 14:24:42 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,20 +23,6 @@ void main() {
   );
 }
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//         visualDensity: VisualDensity.adaptivePlatformDensity,
-//       ),
-//       home: MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
