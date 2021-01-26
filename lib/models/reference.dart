ce0726b2a0fc96dc61558520c5013a8692a24fc1
superior-prog <meherajmahmmd@gmail.com>
2021-01-26 22:50:25 +0600

    Update reference.dart

--- lib/models/reference.dart
+++ lib/models/reference.dart
@@ -7,6 +7,25 @@ class Reference {
   final String email;
   final String phone;
 
-  Reference({this.personId, this.id, this.fullName, this.workPlace, this.designation,
-      this.email, this.phone});
+  Reference({
+    this.personId,
+    this.id,
+    this.fullName,
+    this.workPlace,
+    this.designation,
+    this.email,
+    this.phone,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      "personId": personId,
+      "id": id,
+      "fullName": fullName,
+      "workPlace": workPlace,
+      "designation": designation,
+      "email": email,
+      "phone": phone,
+    };
+  }
 }
