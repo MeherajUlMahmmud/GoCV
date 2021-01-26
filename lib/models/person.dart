acea7e67eee641693cb5b0652e76976fc712001e
superior-prog <meherajmahmmd@gmail.com>
2021-01-26 22:50:28 +0600

    Update person.dart

--- lib/models/person.dart
+++ lib/models/person.dart
@@ -18,63 +18,26 @@ class Person {
   final String facebook;
   final String github;
 
-//  List<Education> educations;
-//  List<Work> works;
-//  List<Language> languages = new List<Language>();
-//  List<Skill> skills = new List<Skill>();
-//  List<Certificate> certificates = new List<Certificate>();
-//  List<Reference> references = new List<Reference>();
-
-  Person(
-      {this.id,
-      this.title,
-      this.firstName,
-      this.surname,
-      this.jobTitle,
-      this.aboutMe,
-      this.gender,
-      this.dob,
-      this.nationality,
-      this.country,
-      this.city,
-      this.creationDateTime,
-      this.phone,
-      this.email,
-      this.address,
-      this.linkedin,
-      this.facebook,
-      this.github});
-
-//  static final Person origin = Person(
-//    "",
-//    "",
-//    "",
-//    "",
-//    "",
-//    "",
-//    "",
-//    "",
-//    "",
-//    "",
-//    "",
-//    "",
-//    "",
-//    "",
-//    "",
-//    "",
-//    "",
-//    new List<Education>(),
-//    new List<Work>(),
-//    new List<Language>(),
-//    new List<Skill>(),
-//    new List<Certificate>(),
-//    new List<Reference>(),
-//  );
-//
-//  // factory constructor
-//  factory Person.create() {
-//    return origin;
-//  }
+  Person({
+    this.id,
+    this.title,
+    this.firstName,
+    this.surname,
+    this.jobTitle,
+    this.aboutMe,
+    this.gender,
+    this.dob,
+    this.nationality,
+    this.country,
+    this.city,
+    this.creationDateTime,
+    this.phone,
+    this.email,
+    this.address,
+    this.linkedin,
+    this.facebook,
+    this.github,
+  });
 
   Map<String, dynamic> toMap() {
     return {
