ab4898c93b12cb7aae9308c397738c6ce81c7cd4
superior-prog <meherajmahmmd@gmail.com>
2021-01-26 22:50:38 +0600

    Update education.dart

--- lib/models/education.dart
+++ lib/models/education.dart
@@ -8,15 +8,16 @@ class Education {
   final String grade;
   final String notes;
 
-  Education(
-      {this.personId,
-      this.id,
-      this.years,
-      this.school,
-      this.faculty,
-      this.department,
-      this.grade,
-      this.notes});
+  Education({
+    this.personId,
+    this.id,
+    this.years,
+    this.school,
+    this.faculty,
+    this.department,
+    this.grade,
+    this.notes,
+  });
 
   Map<String, dynamic> toMap() {
     return {
