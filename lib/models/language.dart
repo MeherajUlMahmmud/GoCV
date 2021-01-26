0dae5a546f064dffecae51cf1b288192990270be
superior-prog <meherajmahmmd@gmail.com>
2021-01-26 22:50:33 +0600

    Update language.dart

--- lib/models/language.dart
+++ lib/models/language.dart
@@ -4,5 +4,19 @@ class Language {
   final String name;
   final int proficiency;
 
-  Language({this.personId, this.id, this.name, this.proficiency});
+  Language({
+    this.personId,
+    this.id,
+    this.name,
+    this.proficiency,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      "personId": personId,
+      "id": id,
+      "name": name,
+      "proficiency": proficiency,
+    };
+  }
 }
