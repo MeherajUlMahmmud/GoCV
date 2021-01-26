95ba2fac7a6365444b62482734dd7fbe666107ae
superior-prog <meherajmahmmd@gmail.com>
2021-01-26 22:50:15 +0600

    Update work.dart

--- lib/models/work.dart
+++ lib/models/work.dart
@@ -1,12 +1,31 @@
 class Work {
   final int personId;
   final int id;
-  final String years;
-  final String company;
   final String jobTitle;
+  final String company;
   final String department;
+  final String years;
   final String notes;
 
-  Work({this.personId, this.id, this.years, this.company, this.jobTitle,
-      this.department, this.notes});
+  Work({
+    this.personId,
+    this.id,
+    this.jobTitle,
+    this.company,
+    this.department,
+    this.years,
+    this.notes,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      "personId": personId,
+      "id": id,
+      "jobTitle": jobTitle,
+      "company": company,
+      "department": department,
+      "years": years,
+      "notes": notes,
+    };
+  }
 }
