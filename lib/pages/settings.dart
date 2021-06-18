708f6ede63c46edbbee72d2b8b06cc45f9d06a34
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-06-18 15:16:25 +0600

    Update settings.dart

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -34,38 +34,31 @@ class _SettingsState extends State<Settings> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 1.0,
-          title: Text(
-            "Settings",
-            style: TextStyle(
-              color: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Settings"
+        )
+      ),
+      body: ListView.separated(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            onTap: items[index]['function'],
+            leading: Icon(
+              items[index]['icon'],
             ),
-          ),
-          centerTitle: true,
-        ),
-        body: ListView.separated(
-          padding: EdgeInsets.symmetric(horizontal: 10),
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          itemCount: items.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              onTap: items[index]['function'],
-              leading: Icon(
-                items[index]['icon'],
-              ),
-              title: Text(
-                items[index]['title'],
-              ),
-            );
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return Divider();
-          },
-        ),
+            title: Text(
+              items[index]['title'],
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider();
+        },
       ),
     );
   }
