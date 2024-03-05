ba7497a3a4711e1ab31cb5116483ac3ef68e78c3
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2024-03-05 15:57:37 +0600

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -6,4 +6,14 @@ class Constants {
   static const String appContactNumber = '+1234567890';
 
   static const String defultAvatarPath = 'assets/avatars/rdj.png';
+
+  static const int HTTP_OK = 200;
+  static const int HTTP_CREATED = 201;
+  static const int HTTP_DELETED = 204;
+  static const int HTTP_NOT_FOUND = 404;
+  static const int HTTP_UNAUTHORIZED = 401;
+  static const int HTTP_FORBIDDEN = 403;
+
+  static const String SESSION_EXPIRED_MSG =
+      'Session expired, please login again';
 }
